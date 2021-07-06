d27287a3aad2147b72ef5c40623d17d9321eb8b8
Lydie Douylliez <lydiedouylliez@MacBook-Pro-de-Lydie-2.local>
2021-07-06 11:56:20 +0200

    exercice 1

---
+++ ex_01/Animal.php
@@ -0,0 +1,35 @@
+<?php
+class Animal {
+        const MAMMAL = 0;
+        const FISH = 1;
+        const BIRD = 2;
+        private $name;
+        private $legs;
+        private $type;
+    public function __construct ($blaze,$nblegs,$genre) {
+        $this->name =$blaze;
+        $this->legs =$nblegs;
+        switch ($this->type) {
+            case Animal::MAMMAL:
+                $this->type ="mammal";
+                break;
+            case Animal::FISH:
+                $this->type ="fish";
+                break;
+            case Animal::BIRD:
+                $this->type ="bird";
+                break;
+        }
+        echo "My name is $blaze and I am $this->type !\n";
+    }
+    public function getName (){
+        return $this->name;
+    }
+    public function getLegs (){
+        return $this->legs;
+    }
+    public function getType (){
+        return $this->type;
+    }
+}
+?>
